709ba95380cd5dcf5e5f8047d4cec8e36dff2505
samirovic92 <s.elidrissi1992@gmail.com>
2021-10-06 23:35:19 +0200

    Refactor

--- src/main/java/com/samirovic/RPNCalculator.java
+++ src/main/java/com/samirovic/RPNCalculator.java
@@ -17,18 +17,6 @@ public class RPNCalculator {
 		this.numbers = new Stack<>();
 	}
 
-	private static String[] TEST_DATA = { "20 5 /", // expected: 4.0
-			"4 2 + 3 - ", // expected: 3.0
-			"3 5 8 * 7 + *", // expected: 141.0
-			"9 SQRT", // expected: 3
-			"5 3 4 2 9 1 MAX", // expected: 9
-			"4 5 MAX 1 2 MAX 5 *" // expected: 25.0
-	};
-
-	public static void main(String[] args) {
-		//TODO: implement calculator and use with test data
-	}
-
 	public Float calculate(String expression) {
 		String[] values = expression.split(" ");
 
